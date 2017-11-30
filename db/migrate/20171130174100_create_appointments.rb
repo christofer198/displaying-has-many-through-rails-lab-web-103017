f1094236cc63f3e336c0fed94d87492b5df2f34e
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-30 18:07:42 +0000

    automatically backed up by learn

--- db/migrate/20171130174100_create_appointments.rb
+++ db/migrate/20171130174100_create_appointments.rb
@@ -3,6 +3,7 @@ class CreateAppointments < ActiveRecord::Migration
     create_table :appointments do |t|
       t.integer :patient_id
       t.integer :doctor_id
+      t.datetime :appt_time
 
       t.timestamps null: false
     end
